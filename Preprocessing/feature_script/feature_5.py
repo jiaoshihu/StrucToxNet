eeef87d090d726858216f6a4eac998facb5ff885
jiaoshihu <86032658+jiaoshihu@users.noreply.github.com>
2025-01-16 23:29:03 +0900

    Add files via upload

---
+++ Preprocessing/feature_script/feature_5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+
+
+
+
+def cal_dssp_feature(file_name, seq_id):
+
+    file_path = './outputs/{}/dssp/{}.dssp'.format(file_name, seq_id)
+
+    with open(file_path, 'r') as fd:
+        fdlines = fd.readlines()
+
+    length = int(fdlines[-1].split()[0])
+    feature_list = [[0 for _ in range(6)] for _ in range(length)] 
+
+
+    for line in fdlines[1:]:
+        line = line.strip().split()
+        res = int(line[0]) - 1
+        alpha = float(line[6])
+        phi = float(line[7])  
+        psi = float(line[8])
+        alphasin = math.sin(math.radians(alpha))
+        alphacos = math.cos(math.radians(alpha))
+        phisin = math.sin(math.radians(phi))
+        phicos = math.cos(math.radians(phi))
+        psisin = math.sin(math.radians(psi))
+        psicos = math.cos(math.radians(psi))
+        feature_list[res] = [alphasin, alphacos, phisin, phicos, psisin, psicos]
+    
+    return feature_list
+
+
+
+
+
+
