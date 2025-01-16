12dd8e6ab34170b09438adeb2fcdccd6489fd182
jiaoshihu <86032658+jiaoshihu@users.noreply.github.com>
2025-01-16 22:41:36 +0900

    Add files via upload

---
+++ feature_script/feature_2.py
@@ -0,0 +1,30 @@
+
+def comput_gem_fea(file_name, seq_id, seq):
+    file_path = './outputs/{}/pdb2rr/{}.pdb2rr'.format(file_name, seq_id)
+    with open(file_path, 'r') as fdr:
+        fdrlines = fdr.readlines()
+
+    N = len(seq)
+
+    countlist = [0 for _ in range(N)]
+    for line in fdrlines:
+        res1 = line.split()[0]
+        res2 = line.split()[1]
+        if((int(res1) > N) or (int(res2) > N)):
+                continue
+        countlist[int(res1)-1] += 1
+        countlist[int(res2)-1] += 1
+    fdr.close()
+    for i in range(len(countlist)):
+        if (countlist[i] > 25):
+            countlist[i] = 1
+        else:
+            countlist[i] /= 25
+    
+    countlist = [[x] for x in countlist]
+
+    return countlist
+
+
+
+
---
+++ feature_script/feature_6.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+import numpy as np
+
+
+def cal_dssp_feature(file_name, seq_id):
+
+    file_path = './outputs/{}/dssp/{}.dssp'.format(file_name, seq_id)
+    with open(file_path, 'r') as fd:
+        fdlines = fd.readlines()
+
+    length = int(fdlines[-1].split()[0])
+    feature_list = [[0 for _ in range(6)] for _ in range(length)] # 1D feature dimension is set 6, may change
+
+
+    for linei in range(2, len(fdlines)-1):
+            line = fdlines[linei].split()
+            line_prev = fdlines[linei-1].split()
+            line_post = fdlines[linei+1].split()
+            
+            res = int(line[0]) - 1
+            cai = [float(line[9]), float(line[10]), float(line[11])]
+            cai_prev = [float(line_prev[9]), float(line_prev[10]), float(line_prev[11])]
+            cai_post = [float(line_post[9]), float(line_post[10]), float(line_post[11])]
+            #提取三个Ca的坐标信息
+            
+            forw = np.subtract(cai_post, cai) #坐标对应元素相减
+            revw = np.subtract(cai_prev, cai)
+
+            forwn = math.sqrt(forw[0] ** 2 + forw[1] ** 2 +  forw[2] ** 2) #计算向量的模长
+            revwn = math.sqrt(revw[0] ** 2 + revw[1] ** 2 +  revw[2] ** 2)
+
+            u_forw = forw/forwn
+            u_revw = revw/revwn
+            #print(u_forw, u_revw)
+
+            feature_list[res][:3] = u_forw 
+            feature_list[res][3:] = u_revw
+            #print(feature_list[res])
+    return feature_list
+
+
+
+
+
