c712640ffc7fbb416d307bff2a0099fd801c2978
jiaoshihu <86032658+jiaoshihu@users.noreply.github.com>
2025-01-19 23:05:23 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+# @Time    : 2024/8/16 9:00
+# @Author  : Jiao Shihu
+# @Email   : shihujiao@163.com
+# @IDE     : PyCharm
+# @FileName: main.py
 
-# In[1]:
-
-
+import re
+import os
 import torch
 import pickle
 import pandas as pd
 import numpy as np
-from model import pepmodel
+from model import StrucToxNet
 from configuration import config as cf
 import torch.nn.functional as F
-from util import util_metric
 from preprocess import  data_processing
 
-
+def load_text_file(fast_file):
+    with open(fast_file) as f:
+        lines = f.read()
+        records = lines.split('>')[1:]
+        seq_data = []
+        for line in records:
+            array = line.split('\n')
+            sequence = re.sub('[^ACDEFGHIKLMNPQRSTUVWYX]','-',''.join(array[1:]).upper())
+            seq_data.append(sequence)
+        return seq_data
 
 config = cf.get_train_config()
 
+sequences_list = load_text_file('./Preprocessing/inputs/test_sequences.txt')
 
-with open('./data/combined_test.pkl', 'rb') as f:
+with open('./Preprocessing/outputs/combined_test.pkl', 'rb') as f:
     test_data = pickle.load(f)
 
-def read_fasta_file(filepath):
-    with open(filepath, "r") as file:
-        lines = [line.strip() for line in file]
-        sequences_dict = {lines[i][1:]: lines[i+1] for i in range(0, len(lines), 2)}
-    return sequences_dict
+test_loader = data_processing.create_data_loaders(test_data,config)
 
 
-
-train_loader, test_loader = data_processing.create_data_loaders(test_data,config)
 
 
 def model_eval(data_iter, model, config):
     label_pred = torch.empty([0], device=config.device)
     label_true = torch.empty([0], device=config.device)
     pred_prob = torch.empty([0], device=config.device)
-
-    val_epoch_loss = 0
     step = 0
-
     model.eval()
     with torch.no_grad():
         for batch in data_iter:
@@ -59,32 +62,27 @@ def model_eval(data_iter, model, config):
 
             step += 1
 
-        metric, roc_data, prc_data = util_metric.caculate_metric(label_pred, label_true, pred_prob)
-
-    return metric, roc_data, prc_data, label_true.cpu().numpy(), pred_prob.cpu().numpy()
+    return label_pred.cpu().numpy(), pred_prob.cpu().numpy()
 
 
-model = pepmodel.EGNNModel(config).to(config.device)
+model = StrucToxNet.EGNNModel(config).to(config.device)
 model.load_state_dict(torch.load('model/final_model.pkl', map_location=torch.device('cpu')))
 
-valid_metric, roc_data, prc_data, true_label, probability = model_eval(test_loader, model, config)
+y_pred, y_pred_prob = model_eval(test_loader, model, config)
 
-roc_data_df = pd.DataFrame({
-    'False Positive Rate': roc_data[0],
-    'True Positive Rate': roc_data[1], })
+for i, seq in enumerate(sequences_list):
+    sequences_list[i] = ''.join(seq)
 
-roc_csv_path = f'./result/roc_test.csv'
-roc_data_df.to_csv(roc_csv_path, index=False)
-
-prc_data_save = {'recall': prc_data[0], 'precision': prc_data[1], 'AP': prc_data[2]}
-np.save(f'./result/prc_data.npy', prc_data_save)
-
-df_proba = pd.DataFrame({
-    'true_label': true_label,
-    'probability': probability})
-
-proba_csv_path = f'./result/prolabel_probability.csv'
-df_proba.to_csv(proba_csv_path, index=False)
-
+results = pd.DataFrame(np.zeros([len(y_pred), 4]), columns=["Seq_ID", "Sequences", "Prediction", "Confidence"])
+for i in range(len(y_pred)):
+    if y_pred[i] == 1:
+        y_prob = str(round(y_pred_prob[i] * 100, 2)) + "%"
+        results.iloc[i, :] = [round(i + 1), sequences_list[i], "Toxic peptide", y_prob]
+    else:
+        y_prob = str(round((1-y_pred_prob[i]) * 100, 2)) + "%"
+        results.iloc[i, :] = [round(i + 1), sequences_list[i], "Non toxic peptide", y_prob]
+os.chdir("result")
+results.to_csv("Prediction_results", index=False)
+print("job finished!")
 
 
